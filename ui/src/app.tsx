77b69fd1bda8d577e8902fd87001aa6f9156804e
Cristiano Battini <106992156+cristianobattini@users.noreply.github.com>
2025-11-28 16:51:29 +0100

    fix: update API client base URL to correct production endpoint

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -23,7 +23,7 @@ type AppProps = {
 };
 
 client.setConfig({
-  baseUrl: 'https://vmtrbc01.northeurope.cloudapp.azure.com/api/',
+  baseUrl: 'https://vmtrbc01b.northeurope.cloudapp.azure.com/api/',
   // baseUrl: 'http://localhost:8000/',
   auth: (auth) => {
     const token = getLocalStorageItem('accessToken');
