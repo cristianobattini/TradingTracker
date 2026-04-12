c6c48a5232ffc3e38bab7917adeb1814ea07cd9b
Cristiano Battini <106992156+cristianobattini@users.noreply.github.com>
2026-04-12 16:24:35 +0200

    Update API_BASE_URL to use environment variable in AccountAvatar component

--- ui/src/layouts/components/account-avatar.tsx
+++ ui/src/layouts/components/account-avatar.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { getAvatarApiUsersUserIdAvatarGet } from "src/client";
 
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.API_BASE_URL;
 
 export const AccountAvatar = ({ handleOpenPopover, size, ...other }: any) => {
     const [avatarFileName, setAvatarFileName] = useState<string | null>();
